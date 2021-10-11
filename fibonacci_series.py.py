a5598f363b77ff6602afe6c5b820f5b790a76feb
Hamza Shaikh <91974950+HamzaShaikh1508@users.noreply.github.com>
2021-10-11 17:33:40 +0530

    Add files via upload

---
+++ fibonacci_series.py.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 17:23:59 2021
+
+@author: hamza_sk
+"""
+nterms = int(input("How many terms? "))
+n1, n2 = 0, 1
+count = 0
+
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
