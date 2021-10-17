fb55d68389e30ab6ae68f8651fe7233b1c8cdc94
Hamza Shaikh <91974950+HamzaShaikh1508@users.noreply.github.com>
2021-10-17 19:53:59 +0530

    Add files via upload

---
+++ operators.py.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 17 19:48:33 2021
+
+@author: hamza_sk
+"""
+
+#operators
+x=int(input("Enter first number : "))
+y=int(input("Enter second number : "))
+print(x<y)
+print(x<=2)
+print(x>y)
+print(x>=0)
+not print(x==y)
+
